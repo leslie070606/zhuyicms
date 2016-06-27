56c661fe116aa21670058dadea208e3467274707
yhl27ml@126.com <yhl27ml@126.com>
2016-06-27 19:46:17 +0800

    Revert "Revert "Revert "前端创建项目保存"""
    This reverts commit feebce2e3741017c8011f6e50780fdf80d1e4fdb.

--- frontend/controllers/ProjectController.php
+++ frontend/controllers/ProjectController.php
@@ -8,20 +8,27 @@ use backend\models\Project;
 class ProjectController extends \common\util\BaseController {
 
     public function actionRequirement() {
-
         $model = new Project();
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['select-designer', 'project_id' => $model->id]);
-        } else {
-            return $this->render('requirement', [
-                        'model' => $model,
-            ]);
-        }
+        return $this->render('requirement', [
+                    'model' => $model,
+        ]);
     }
 
-    public function actionSelectDesigner($project_id) {
-        echo "OK";
+    public function actionCreate() {
+        if(!Yii::$app->request->isAjax) {
+            return;
+        }
+        
+        $model = new Project();
+        
+        $model->load(Yii::$app->request->post());
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            $this->sendRes(true, 'ok');
+        } else {
+            var_dump($model->getErrors());
+        }
     }
 
 }
