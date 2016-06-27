94e1889d00e8318c6e7e42c3801b61ca3e4c9ee9
yhl27ml@126.com <yhl27ml@126.com>
2016-06-27 19:31:36 +0800

    Revert "前端创建项目保存"
    This reverts commit 0a1d527712f8f74e218d32299659bfb514f53a02.

--- backend/models/Project.php
+++ backend/models/Project.php
@@ -6,20 +6,12 @@ class Project extends \common\models\ZyjProject {
 
     public function rules() {
         return [
-            [['house_type', 'completion_time', 'room_area', 'budget_ceiling', 'service_item'], 'required'],
-            [['uid', 'province', 'city', 'house_type', 'completion_time', 'status', 'createtime', 'updatetime'], 'integer'],
+            [['uid', 'province', 'city', 'house_type', 'completion_time', 'room_area', 'budget_ceiling', 'service_item', 'generic_require', 'description', 'residential_district', 'photo', 'status', 'createtime', 'updatetime'], 'required'],
+            [['uid', 'province', 'city', 'house_type', 'completion_time', 'service_item', 'generic_require', 'status', 'createtime', 'updatetime'], 'integer'],
             [['description'], 'string'],
             [['room_area', 'budget_ceiling', 'residential_district'], 'string', 'max' => 20],
-            [['service_item', 'generic_require'], 'string', 'max' => 30],
             [['photo'], 'string', 'max' => 255],
         ];
-    }
-
-    public function beforeValidate() {
-        $this->service_item = is_array($this->service_item) ? ',' . implode(',', $this->service_item) . ',' : $this->service_item;
-        $this->generic_require = is_array($this->generic_require) ? ',' . implode(',', $this->generic_require) . ',' : $this->generic_require;
-
-        return parent::beforeValidate();
     }
 
 }
