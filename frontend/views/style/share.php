2bb0315a177feaa68825b51963c434a16779378f
peng zhao <peng.zhao@condenast.com.cn>
2016-07-04 16:46:29 +0800

    上传
    上传

--- frontend/views/style/share.php
+++ frontend/views/style/share.php
@@ -6,18 +6,24 @@
     </head>
     <body>
         <script type="text/javascript">
+
             wx.config({
-                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: 'wx1344a7a9fac82094', // 必填，公众号的唯一标识
                 timestamp: <?= $jsarr['timestamp'] ?>, // 必填，生成签名的时间戳
                 nonceStr: 'zhuyi', // 必填，生成签名的随机串
                 signature: "<?= $jsarr['signature'] ?>", // 必填，签名，见附录1
-                jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline ', 'checkJsApi'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline', 'checkJsApi', 'chooseImage', 'uploadImage', 'downloadImage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
             });
-
+            //定义images用来保存选择的本地图片ID，和上传后的服务器图片ID
+            var images = {
+                localId: [],
+                serverId: []
+            };
             wx.ready(function () {
+               
                 wx.checkJsApi({
-                    jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'checkJsApi'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+                    jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'checkJsApi', 'chooseImage', 'uploadImage', 'downloadImage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
                     success: function (res) {
                         //alert(res);
                         // 以键值对的形式返回，可用的api值true，不可用为false
@@ -48,7 +54,7 @@
                 //分享到朋友圈
                 wx.onMenuShareTimeline({
                     title: '分享给你的朋友', // 分享标题
-                    link: '#', // 分享链接
+                    link: 'http://puti.kim/index.php?r=style/share', // 分享链接
                     imgUrl: '#', // 分享图标
                     success: function () {
                         // 用户确认分享后执行的回调函数
@@ -61,10 +67,46 @@
                         alert(JSON.stringify(res));
                     }
                 });
+                //var ;
+                // 拍照选择图片
+                wx.chooseImage({
+                    count: 9, // 默认9
+                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                    success: function (res) {
+                        images.localId = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+
+                    }
+                });
+                document.getElementById('upload').onclick = function () {
+                    alert(88888);
+                    // 上传图片
+                    wx.uploadImage({
+                        localId: images.localId, // 需要上传的图片的本地ID，由chooseImage接口获得
+                        isShowProgressTips: 1, // 默认为1，显示进度提示
+                        success: function (res) {
+//                        var serverId = res.serverId; // 返回图片的服务器端ID
+//                        var html = document.getElementById("a");
+//                        html.innerHTML = serverId;
+                            alert(123);
+
+                        }
+                    });
+                }
+
+//                wx.downloadImage({
+//                    serverId: '1237378768e7q8e7r8qwesafdasdfasdfaxss111', // 需要下载的图片的服务器端ID，由uploadImage接口获得
+//                    isShowProgressTips: 1, // 默认为1，显示进度提示
+//                    success: function (res) {
+//                        var localId = res.localId; // 返回图片下载后的本地ID
+//                        alert(localId);
+//                    }
+//                });
             })
 
         </script>
-        <span style="font-size: 45px; font-weight: 15px;">逗你玩!</span>
+        <button id="upload" style="display: block;margin: 2em auto; width: 200px; height: 50px;">上传接口测试</button>
+        <span style="font-size: 45px; font-weight: 15px;" id="a">逗你玩!</span>
     </body>
 </html>
 
