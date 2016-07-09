1f32ba4f16c609d6b7a29c55688488048dc9d998
qixinliang <335676770@qq.com>
2016-07-09 14:22:07 +0800

    order status

--- frontend/models/Order.php
+++ frontend/models/Order.php
@@ -4,18 +4,29 @@ use yii\db\ActiveRecord;
 
 class Order extends ActiveRecord{
 	private $_field = null;  
-	const STATUS_DISABLED 						= -1; //无效
-	const STATUS_NOT_INIT 						= 0;  //未初始化 
-	const STATUS_INIT 							= 1;  //初始化
-	const STATUS_USER_DEMAND_NOT_SUBMITTED 		= 2;  //等待用户提交需求
-	const STATUS_MATCHING_DESIGNER 				= 3;  //正在匹配设计师
-	const STATUS_WAITING_FOR_USER_CONFIRM_TIME 	= 4;  //等待用户确认时间
-	const STATUS_USER_TIME_CONFIRMED 			= 5;  //用户已确认时间
-	const STATUS_WAITING_MEETING 				= 6;  //等待约见
-	const STATUS_MET_NOT_DEEP_COOPERATION 		= 7;  //已见面未深度合作
-	const STATUS_WAITING_CONTRACT 				= 8;  //深度合作等待合同上传中
-	const STATUS_SERVICE_END					= 9;  //合作完成
-	const STATUS_SERVICE_CANCELLED				= 10; //取消合作
+	const STATUS_DISABLED 						= -1;
+	const STATUS_WAITING_DESIGNER_TO_CONFIRM	= 0;
+	const STATUS_WAITING_USER_TO_CONFIRM_TIME	= 1;
+	const STATUS_WAITING_MEETING 				= 2;
+	const STATUS_CANCEL_MEETING 				= 3;
+	const STATUS_NOT_TO_MEET					= 4;
+	const STATUS_MET_NOT_DEEP_COOPERATION		= 5;
+	const STATUS_WAITING_CONTRACT				= 6;
+	const STATUS_SERVICE_END					= 7;
+	const STATUS_SERVICE_CANCELLED				= 8;
+
+	static $ORDER_STATUS_DICT = array(
+		self::STATUS_DISABLED 						=> '无效订单',
+		self::STATUS_WAITING_DESIGNER_TO_CONFIRM 	=> '待设计师确认',
+		self::STATUS_WAITING_USER_TO_CONFIRM_TIME 	=> '待用户确认时间',
+		self::STATUS_WAITING_MEETING				=> '待见面',
+		self::STATUS_CANCEL_MEETING					=> '取消约见',
+		self::STATUS_NOT_TO_MEET					=> '不见面',
+		self::STATUS_MET_NOT_DEEP_COOPERATION		=> '已见面未深度合作',
+		self::STATUS_WAITING_CONTRACT				=> '已深度合作未上传合同',
+		self::STATUS_SERVICE_END					=> '已深度合作已上传合同',
+		self::STATUS_SERVICE_CANCELD				=> '取消合作'
+	);
 
 	public function __construct(){
 		parent::__construct();
