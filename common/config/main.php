903fde40e9e62441179e8003dcfaf1087f3f5132
zhaopeng38 <526080515@qq.com>
2016-07-19 19:13:25 +0800

    Update main.php

--- common/config/main.php
+++ common/config/main.php
@@ -9,11 +9,21 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
     ],
-    'db' => [
-        'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=101.201.110.46;dbname=zhuyicms',
-        'username' => 'root',
-        'password' => '526080515',
-        'charset' => 'utf8',
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=101.201.110.46;dbname=zhuyicms',
+            'username' => 'root',
+            'password' => '526080515',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
     ],
 ];
