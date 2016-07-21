d966a60576557901ff01380b2610b1dfaeb3b36d
peng zhao <peng.zhao@condenast.com.cn>
2016-07-21 19:57:27 +0800

    加入query

--- backend/views/zyartsets/_formmy.php
+++ backend/views/zyartsets/_formmy.php
@@ -2,39 +2,21 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use kartik\file\FileInput;
 
-/*
-<link href=<?php echo Yii::getAlias('@web')."css/diyUpload.css"?> />
-*/?>
-<script type="text/javascript" src="js/diyUpload.js"></script>
-<script type="text/javascript" src="js/webuploader.html5only.min.js"></script>
+/* @var $this yii\web\View */
+/* @var $model common\models\ZyArtsets */
+/* @var $form yii\widgets\ActiveForm */
+?>
 
-
-<script>
-$('#test').diyUpload({
-	url:'server/fileupload.php',
-	success:function( data ) {
-		console.info( data );
-	},
-	error:function( err ) {
-		console.info( err );	
-	}
-});
-
-</script>
 <div class="zy-artsets-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'designer_id')->textInput() ?>
-	<div id="test"></div>
-    <?= $form->field($model, 'image_ids')->widget(FileInput::classname(),['options' => ['multiple' => true]]) ?>
-	<?php
-	/*
 
-    <?= $form->field($model, 'video_ids')->widget(FileInput::classname(),['options' => ['multiple' => true]]) ?>
+    <?= $form->field($model, 'image_ids')->textInput(['maxlength' => true]) ?>
 
+    <?= $form->field($model, 'video_ids')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'topic')->textInput(['maxlength' => true]) ?>
 
@@ -61,8 +43,6 @@ $('#test').diyUpload({
     <?= $form->field($model, 'create_time')->textInput() ?>
 
     <?= $form->field($model, 'update_time')->textInput() ?>
-	*/
-	?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
