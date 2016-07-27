e21bb0f1f7d28b7ba11c528e1f1edc57dae1626a
peng zhao <peng.zhao@condenast.com.cn>
2016-07-27 13:48:17 +0800

    ok

--- frontend/views/project/additional.php
+++ frontend/views/project/additional.php
@@ -125,56 +125,141 @@ use yii\helpers\Url;
         var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
 
         touch.on(".add_img", "tap", function (ev) {
-            var _this = $(this);
-            var html = "";
-            var likestr = "";
-            var i = 0;
+            var _this = $(ev.currentTarget);
+            var index;
             if (_this.parents("ul").attr("id") == "ulb") {
-                upload($("#ulb"), $(".like"),i)
-
+                index = 1;
             } else {
-                upload($("#ula"), $(".home"),i)
+                index = 0;
             }
 
+            var length = $(ev.currentTarget).siblings().length;
+            var likestr = '';
+            var nuber = 0;
 
+            // 拍照选择图片
+            wx.chooseImage({
+                count: 9, // 默认9
+                sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                success: function (res) {
+                    images.localId = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
 
+                    var html = "";
+                    // alert(indexx)
+                    if (index == 1) {
 
+                        for (var i = 0; i < images.localId.length; i++) {
+                            var htmllll = images.localId[i];
+                            html += '<li><img src="' + htmllll + '" value=""> <i class="iconfont icon-shanchu"></i></li>';
+                            var ge = images.localId.length;
+                            // 上传图片
+                            wx.uploadImage({
+                                localId: images.localId[i], // 需要上传的图片的本地ID，由chooseImage接口获得
+                                isShowProgressTips: 1, // 默认为1，显示进度提示
+                                success: function (res) {
+                                    serverId = res.serverId; // 返回图片的服务器端ID
+                                    likestr += serverId + "$";
+
+                                    nuber++;
+                                    if (isAndroid) {
+                                        if (nuber >= ge) {
+                                            var like_val = $(".like").val();
+                                            $(".like").val(like_val + likestr);
+
+                                            likestr = likestr.toString().split("$");
+                                            for (var i = 0; i < $("#ulb li").length - 1; i++) {
+                                                $("#ulb li:eq(" + i + ") ").prop("img_id", likestr[i]);
+                                            }
+                                        }
+                                    } else {
+
+                                    uploadIOS(images.localId[i],i);
+                                        //alert(nuber);
+
+                                        var like_val = $(".like").val();
+                                        $(".like").val(like_val + likestr);
+
+                                        likestr = likestr.toString().split("$");
+                                        for (var i = 0; i < $("#ulb li").length - 1; i++) {
+                                            $("#ulb li:eq(" + i + ") ").prop("img_id", likestr[i]);
+                                        }
+
+                                    }
+
+                                }
+                            });
+                        }
+
+                        $("#ulb").prepend(html);
+                        //alert($(".home").val());
+                        alert($(".like").val());
+                        img_height_auto();
+                        auto_click();
+                    } else {
+                        for (var i = 0; i < images.localId.length; i++) {
+                            var htmllll = images.localId[i];
+                            html += '<li><img src="' + htmllll + '" value=""> <i class="iconfont icon-shanchu"></i></li>';
+                            var ge = images.localId.length;
+                            // 上传图片
+                            wx.uploadImage({
+                                localId: images.localId[i], // 需要上传的图片的本地ID，由chooseImage接口获得
+                                isShowProgressTips: 1, // 默认为1，显示进度提示
+                                success: function (res) {
+                                    serverId = res.serverId; // 返回图片的服务器端ID
+                                    likestr += serverId + "$";
+                                    nuber++;
+                                    if (isAndroid) {
+                                        if (nuber >= ge) {
+                                            var like_val = $(".home").val();
+                                            $(".home").val(like_val + likestr);
+
+                                            likestr = likestr.toString().split("$");
+                                            for (var i = 0; i < $("#ula li").length - 1; i++) {
+                                                $("#ula li:eq(" + i + ") ").prop("img_id", likestr[i]);
+                                            }
+                                        }
+                                    } else {
+                                        var like_val = $(".home").val();
+                                        $(".home").val(like_val + likestr);
+                                        //alert($(".home").val())
+                                        likestr = likestr.toString().split("$");
+                                        for (var i = 0; i < $("#ula li").length - 1; i++) {
+                                            $("#ula li:eq(" + i + ") ").prop("img_id", likestr[i]);
+                                        }
+                                    }
+                                }
+                            });
+                        }
+
+                        $("#ula").prepend(html);
+                        alert($(".home").val());
+                        alert($(".like").val());
+                        img_height_auto();
+                        auto_click();
+                    }
+
+                }
+
+            })
         });
 
     });
 
-// 拍照选择图片
 
-    function upload(ul_id, val_class,i) {
+    function uploadIOS(localId,i) {
         wx.uploadImage({
-            count: 9, // 默认9
-            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-            localId: images.localId[i], // 需要上传的图片的本地ID，由chooseImage接口获得
+            localId: localId,
             success: function (res) {
-                likestr += serverId + "$";
-                html += '<li><img src="' + localId + '" value=""> <i class="iconfont icon-shanchu"></i></li>';
+                serverIds.push(res.serverId);
                 i++;
                 if (i < length) {
-                    upload(ul_id, val_class);
+                    upload(localId,i);
                 } else {
-                    var like_val = val_class.val();
-                    val_class.val(like_val + likestr);
-                    likestr = likestr.toString().split("$");
-                    for (var b = 0; b < ul_id.find("li").length - 1; b++) {
-                        ul_id.find("li:eq(" + b + ") ").prop("img_id", likestr[b]);
-
-                    }
-                    ul_id.prepend(html);
-                    img_height_auto();
-                    auto_click();
+                     alert("serverIds" + serverIds);
                 }
-            },
-            fail: function () {
-                alert("upload fail please wait a moment and try again");
             }
-        });
-
+        })
     }
-    ;
+
 </script>
