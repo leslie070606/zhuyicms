13b7ec38b63a9811eb2049e17dda02431f27ad42
peng zhao <peng.zhao@condenast.com.cn>
2016-08-09 17:52:29 +0800

    首页

--- frontend/controllers/IndexController.php
+++ frontend/controllers/IndexController.php
@@ -9,19 +9,7 @@ class IndexController extends Controller {
     public $layout = 'zhuyimain';
 
     public function actionIndex() {
-//        //初始化session
-//        $session = Yii::$app->session;
-//        if (!$session->isActive) {
-//            $session->open();
-//        }
-//        //session_set_cookie_params(10);
-//        if($session->get('user_id')){
-//            echo $session->get('user_id');
-//            exit;
-//        }else{
-//            echo 'no login!';
-//            exit;
-//        }
+
         $model = new \common\models\ZyIndex();
         $video = $model->find()->orderBy('sort asc')->all();
 
