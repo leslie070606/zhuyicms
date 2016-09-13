b2bab5fd079c4051dcd8f75cacf9fc349024d33b
shuaiwei.hao <shuaiwei.hao@WSBEI101.condenast.com.cn>
2016-09-13 17:14:51 +0800

    订单count，及需求到设计师跳转

--- backend/controllers/ZyorderController.php
+++ backend/controllers/ZyorderController.php
@@ -55,7 +55,7 @@ class ZyorderController extends Controller {
         $query->select("zy_user.nickname,zyj_designer_basic.name,zy_order.*");
         $w = $this->_getZyOrderIndexSearch();
         $query->where($w)->orderBy(' order_id desc ');
-        $pages = new \yii\data\Pagination(['totalCount' => $query->count(), 'pageSize' => '15']);
+        $pages = new \yii\data\Pagination(['totalCount' => $query->count('order_id'), 'pageSize' => '15']);
         $model = $query->offset($pages->offset)->limit($pages->limit)->all();
 
         return $this->render('index', [
