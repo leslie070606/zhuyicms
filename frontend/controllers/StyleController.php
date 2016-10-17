392c4d1c238f6a0c442fc83fd4dc864952eaa126
peng zhao <peng.zhao@condenast.com.cn>
2016-10-17 20:33:15 +0800

    3

--- frontend/controllers/StyleController.php
+++ frontend/controllers/StyleController.php
@@ -122,7 +122,7 @@ class StyleController extends Controller {
                     if ($res['open_id'] == $userinfo['openid']) {
                         $frindstyle = $shareModel->findAll(['source_openid' => $link_id]);
                         $mystyle = $shareModel->findOne(['link_id' => $link_id]);
-                        return $this->render('mytestdata', ['mystyle' => $mystyle, 'frindstyle' => $frindstyle, 'link_id' => $link_id]);
+                        return $this->render('mytestdata', ['mystyle' => $mystyle,'jsarr' => $jsarr, 'frindstyle' => $frindstyle, 'link_id' => $link_id]);
                     } else {
                         // 有ID说明朋友在测试
                         if (isset($flashid) && !empty($flashid)) {
