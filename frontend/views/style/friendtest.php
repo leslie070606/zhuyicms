7d177baaac0dd369a50767067d7d0310c249c813
peng zhao <peng.zhao@condenast.com.cn>
2016-10-18 01:09:22 +0800

    背景颜色

--- frontend/views/style/friendtest.php
+++ frontend/views/style/friendtest.php
@@ -9,34 +9,55 @@
         <link rel="stylesheet" type="text/css" href="css/problem.css"  />
     </head>
     <body>
-        <div class="self_foin">
-            <div class="foin_top">
+        
                 <?php
                 switch ($style) {
                     case '波西米亚' :
+                        echo '<div class="self_foin" style="background-color: #f9e8d9;">
+            <div class="foin_top">';
                         echo "<img src='img/fenggejieguo/boximiya01.png' />";
                         break;
                     case '中古' :
+                        echo '<div class="self_foin" style="background-color: #d2d0cf;">
+            <div class="foin_top">';
                         echo "<img src='img/fenggejieguo/zhonggu01.png' />";
                         break;
                     case '法式古典' :
+                         echo '<div class="self_foin" style="background-color: #c6a885;">
+            <div class="foin_top">';
                         echo "<img src='img/fenggejieguo/fashigudian01.png' />";
                         break;
                     case '工业' :
+                         echo '<div class="self_foin" style="background-color: #838384;">
+            <div class="foin_top">';
                         echo "<img src='img/fenggejieguo/gongye01.png' />";
                         break;
                     case '美式' :
+                         echo '<div class="self_foin" style="background-color: #cccccc;">
+            <div class="foin_top">';
                         echo "<img src='img/fenggejieguo/meishi01.png' />";
                         break;
                     case '和式' :
+                         echo '<div class="self_foin" style="background-color: #f5e9de;">
+            <div class="foin_top">';
                         echo "<img src='img/fenggejieguo/heshi01.png' />";
                         break;
                     case '现代简约' :
+                         echo '<div class="self_foin" style="background-color: #dadada;">
+            <div class="foin_top">';
                         echo "<img src='img/fenggejieguo/xiandaijianyue01.png' />";
                         break;
                     case '新中式' :
+                         echo '<div class="self_foin" style="background-color: #884d4d;">
+            <div class="foin_top">';
                         echo "<img src='img/fenggejieguo/xinzhongshi01.png' />";
                         break;
+                    default :
+                        case '波西米亚' :
+                        echo '<div class="self_foin" style="background-color: #f9e8d9;">
+            <div class="foin_top">';
+                        echo "<img src='img/fenggejieguo/boximiya01.png' />";
+                        
                 }
                 ?>
             </div>
