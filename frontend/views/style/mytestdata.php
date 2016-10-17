8748d1229053903e1fabe83fe4fc2eabda5eb6cf
peng zhao <peng.zhao@condenast.com.cn>
2016-10-17 17:49:05 +0800

    123

--- frontend/views/style/mytestdata.php
+++ frontend/views/style/mytestdata.php
@@ -16,22 +16,24 @@ use yii\helpers\Url;
             <div class="chose_top">
                 <div class="top_mesg">
                     <span class="top_mesg_name"><?= $mystyle['user_name'] ?></span>
-                    <span>你酷感十足，你是机械时代的迷恋者，你是拒绝平庸的摇滚先锋，</span>
+<!--                    <span>你酷感十足，你是机械时代的迷恋者，你是拒绝平庸的摇滚先锋，</span>
                     <span>你最喜爱的风格是——工业风格</span>
-                </div>
+                </div>-->
                 <?php
                 switch ($mystyle['style']) {
                     case '波西米亚' :
-                        echo "<img src='img/fenggejieguo/boximiya_bg.png' />";
+                        
                         echo "<span>你叛逆不羁,你是异域风情的制造者,你是自由不羁的Bobo族</span>
                     <span>你最喜爱的风格是——波西米亚风</span>
                 </div>";
+                        echo "<img src='img/fenggejieguo/boximiya_bg.png' />";
                         break;
                     case '中古' :
-                        echo "<img src='img/fenggejieguo/zhonggu_bg.png' />";
+                        
                         echo "<span>你复古时尚,你是摩登时期的缅怀者,你是擅用经典的创想家</span>
                     <span>你最喜爱的风格是——中古风格</span>
                 </div>";
+                        echo "<img src='img/fenggejieguo/zhonggu_bg.png' />";
                         break;
                     case '法式古典' :
                         echo "<img src='img/fenggejieguo/fashigudian_bg.png' />";
@@ -40,40 +42,46 @@ use yii\helpers\Url;
                 </div>";
                         break;
                     case '工业' :
-                        echo "<img src='img/fenggejieguo/gongye_bg.png' />";
+                        
                         echo "<span>你酷感十足,你是机械时代的迷恋者,你是拒绝平庸的摇滚先锋</span>
                     <span>你最喜爱的风格是——工业风格</span>
                 </div>";
+                        echo "<img src='img/fenggejieguo/gongye_bg.png' />";
                         break;
                     case '美式' :
-                        echo "<img src='img/fenggejieguo/meishi_bg.png' />";
+                        
                         echo "<span>你兼容并蓄,你是浪漫怀古的品位缔造者,你是追求自然舒适的生活家</span>
                     <span>你最喜爱的风格是——美式风格</span>
                 </div>";
+                        echo "<img src='img/fenggejieguo/meishi_bg.png' />";
                         break;
                     case '和式' :
-                        echo "<img src='img/fenggejieguo/heshi_bg.png' />";
+                        
                         echo "<span>你自在朴素,你是侘寂美学的敬仰者,你是禅意生活的实践家</span>
                     <span>你最喜爱的风格是——和式风格</span>
                 </div>";
+                        echo "<img src='img/fenggejieguo/heshi_bg.png' />";
                         break;
                     case '现代简约' :
-                        echo "<img src='img/fenggejieguo/xiandaijianyue_bg.png' />";
+                        
                         echo '<span>你简单纯粹,你是“Less is More”的信徒,你是追寻本质的思想者</span>
                     <span>你最喜爱的风格是——现代简约风格</span>
                 </div>';
+                        echo "<img src='img/fenggejieguo/xiandaijianyue_bg.png' />";
                         break;
                     case '新中式' :
-                        echo "<img src='img/fenggejieguo/xinzhongshi_bg.png' />";
+                        
                         echo "<span>你清雅含蓄,你是中国传统文化的研习者,你是东方智慧的传承人</span>
                     <span>你最喜爱的风格是——新中式风格</span>
                 </div>";
+                        echo "<img src='img/fenggejieguo/xinzhongshi_bg.png' />";
                         break;
                     default :
-                        echo "<img src='img/fenggejieguo/boximiya_bg.png' />";
+                        
                         echo "<span>你叛逆不羁,你是异域风情的制造者,你是自由不羁的Bobo族</span>
                     <span>你最喜爱的风格是——波西米亚风</span>
                 </div>";
+                        echo "<img src='img/fenggejieguo/boximiya_bg.png' />";
                 }
                 ?>
             </div>
